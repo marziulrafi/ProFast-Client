79fb7de17110a4899fe9789dcba67af47235971d
K M Marziul Karim <marziulrafi@gmail.com>
2025-06-19 23:14:12 +0600

    Brands added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../components/Banner';
 import Services from '../components/Services';
+import Brands from '../components/Brands';
 
 const Home = () => {
     return (
         <div>
-           <Banner/>
-           <Services/>
+            <Banner />
+            <Services />
+            <Brands />
         </div>
     );
 };
